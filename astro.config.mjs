b4b4a866588584d66fa7db7d61170d34bc8fa51e
herve-ch <rv_changue@hotmail.com>
2022-12-25 23:42:10 -1000

    ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify/functions";
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
 	integrations: [mdx(), tailwind()],
+	adapter: netlify()
 });
